8cce9bd31a1a662ece99f7f68c304e47c55abfb7
İrem Doğrusöz <dogrusozirem11@gmail.com>
2023-03-10 20:27:00 +0300

    Update diabetes_prediction.py

--- diabetes/diabetes_prediction.py
+++ diabetes/diabetes_prediction.py
@@ -1,14 +1,20 @@
 import joblib
 import pandas as pd
 
+# Load the dataset
 df = pd.read_csv("datasets/diabetes.csv")
 
+# Choose a random patient from the dataset
 random_user = df.sample(1, random_state=45)
+
+# Load the trained model
 new_model = joblib.load("voting_clf.pkl")
+
+# Make a prediction for the chosen patient.
 new_model.predict(random_user)
 
 from diabetes_pipeline import diabetes_data_prep
-
+# Prepare the data before prediction
 X, y = diabetes_data_prep(df)
 random_user = X.sample(1, random_state=50)
 new_model = joblib.load("voting_clf.pkl")
